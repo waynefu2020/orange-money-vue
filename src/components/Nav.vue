6705cc363c9d0c6f975ae045d08de722d5f5f09b
DDWayne <652507565@qq.com>
2021-06-02 14:40:43 +0800

    添加Nav样式

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,21 +1,46 @@
 <template>
-  <div class="nav">
-    <router-link to="/money">记账</router-link>
-    |
-    <router-link to="/labels">标签</router-link>
-    |
-    <router-link to="/statistics">统计</router-link>
-  </div>
+  <nav>
+    <router-link to="/money" class="item">
+      <Icon name="money"/>
+      记账
+    </router-link>
+    <router-link to="/labels" class="item">
+      <Icon name="label"/>
+      标签
+    </router-link>
+    <router-link to="/statistics" class="item">
+      <Icon name="statistics"/>
+      统计
+    </router-link>
+  </nav>
 </template>
 
 <script lang="ts">
+  import Icon from "@/components/Icon.vue";
+
   export default {
-    name: "Nav"
+    name: "Nav",
+    components:{Icon}
   }
 </script>
 
 <style lang="scss" scoped>
-  .nav{
-    border: 1px solid red;
+  nav{
+    display: flex;
+    flex-direction: row;
+    box-shadow: 0 0 3px rgba(0,0,0,0.15);
+    font-size: 12px;
+    > .item{
+      padding: 2px 0;
+      width: 33.3333333%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+      .icon{
+        height: 28px;
+        width: 28px;
+      }
+    }
   }
 </style>
