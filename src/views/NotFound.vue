6dfd0a94f7002a67b495e525e47088ca2a2ed1ba
DDWayne <652507565@qq.com>
2021-06-02 10:48:48 +0800

    添加404页面

---
+++ src/views/NotFound.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <span>页面没找到！</span>
+    <div>
+      <router-link to="/">返回首页</router-link>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "NotFound"
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
