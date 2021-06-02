43d4ac03dfd6c600ececc776cfe3ac97a6d61d5c
DDWayne <652507565@qq.com>
2021-06-02 11:00:29 +0800

    Money页面布局

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,16 +1,26 @@
 <template>
-  <div>
-    Money.vue
+  <div class="nav-wrapper">
+    <div class="content">Money.vue</div>
     <Nav/>
   </div>
 </template>
 
 <script lang="ts">
-  export default {
-    name: "Money",
-  }
+export default {
+  name: "Money",
+}
 </script>
 
 <style lang="scss" scoped>
+.nav-wrapper {
+  border: 1px solid green;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
 
+.content {
+  overflow: auto;
+  flex-grow: 1;
+}
 </style>
