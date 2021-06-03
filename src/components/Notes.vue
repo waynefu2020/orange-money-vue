7265f7f1a5efcd7493be7759921e48b7fc2aef3f
DDWayne <652507565@qq.com>
2021-06-03 18:11:23 +0800

    完成note组件功能

--- src/components/Notes.vue
+++ src/components/Notes.vue
@@ -1,16 +1,27 @@
 <template>
   <div>
     <label class="notes">
+      {{value}}
       <span class="name">备注</span>
-      <input type="text" placeholder="添加备注...">
+      <input type="text" :value="value"
+             @input="onChange"
+             placeholder="添加备注...">
     </label>
   </div>
 
 </template>
 
 <script lang="ts">
-export default {
-  name: "Notes"
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+
+@Component
+export default class Notes extends Vue{
+  value = '';
+  onChange(event: KeyboardEvent){
+    const input = event.target as HTMLInputElement
+    this.value = input.value
+  }
 }
 </script>
 
