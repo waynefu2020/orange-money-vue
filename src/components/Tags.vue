886767c6cd94744d5a8166698561efd578edf32a
DDWayne <652507565@qq.com>
2021-06-03 16:01:47 +0800

    Money.vue 分成 4 个子组件

---
+++ src/components/Tags.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="tags">
+    <div class="new">
+      <button>新增标签</button>
+    </div>
+    <ul class="current">
+      <li>衣服</li>
+      <li>住房</li>
+      <li>饮食</li>
+      <li>交通</li>
+      <li>衣服</li>
+      <li>住房</li>
+      <li>饮食</li>
+      <li>交通</li>
+      <li>衣服</li>
+      <li>住房</li>
+      <li>饮食</li>
+      <li>交通</li>
+      <li>衣服</li>
+      <li>住房</li>
+      <li>饮食</li>
+      <li>交通</li>
+      <li>衣服</li>
+      <li>住房</li>
+      <li>饮食</li>
+      <li>交通</li>
+    </ul>
+
+  </div>
+
+</template>
+
+<script lang="ts">
+export default {
+  name: "Tags"
+}
+</script>
+
+<style lang="scss" scoped>
+.tags {
+  display: flex;
+  flex-grow: 1;
+  font-size: 14px;
+  padding: 16px;
+  flex-direction: column-reverse;
+
+  > .current {
+    display: flex;
+    flex-wrap: wrap;
+
+    > li {
+      background: #d9d9d9;
+      $h: 24px;
+      height: $h;
+      border-radius: 12px;
+      padding: 0 16px;
+      margin-right: 12px;
+      margin-top: 4px;
+    }
+  }
+
+  > .new {
+    padding-top: 16px;
+
+    button {
+      background: transparent;
+      border: none;
+      border-bottom: 1px solid;
+      color: #999;
+      padding: 0 4px;
+    }
+  }
+
+  &.selected {
+  }
+}
+
+</style>
