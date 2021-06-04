ad9dc7a69802fd0e1863b343e5b5711eb2caf36d
DDWayne <652507565@qq.com>
2021-06-04 16:16:06 +0800

    封装clone、fetch和save到model.ts

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,10 +1,10 @@
 <template>
   <Layout class-prefix="layout">
-    <NumberPad :value.sync="record.amount"/>
+    <NumberPad :value.sync="record.amount" @submit="saveRecord"/>
     <Types :value.sync="record.type"/>
     <Notes @update:value="onUpdateNotes"/>
     <Tags :data-source.sync="tags" @update:value="onUpdateTags"/>
-    {{ record }}
+    {{ recordList }}
   </Layout>
 </template>
 
@@ -14,20 +14,25 @@ import Types from '@/components/Types.vue';
 import Notes from '@/components/Notes.vue';
 import Tags from '@/components/Tags.vue';
 import Vue from 'vue';
-import {Component} from 'vue-property-decorator';
+import {Component, Watch} from 'vue-property-decorator';
+import model from '@/model';
 
-type Record = {
+const recordList: RecordItem[] = model.fetch();
+
+type RecordItem = {
   tags: string[],
   notes: string,
-  type: string
-  amount: number
+  type: string,
+  amount: number,
+  createAt?: Date
 }
+
 
 @Component({components: {NumberPad, Types, Notes, Tags}})
 export default class Money extends Vue {
   tags = ['衣', '食', '住', '行', '娱乐'];
-
-  record: Record = {tags: [], notes: '', type: '-', amount: 0};
+  recordList: RecordItem[] = recordList;
+  record: RecordItem = {tags: [], notes: '', type: '-', amount: 0};
 
   onUpdateTags(value: string[]) {
     this.record.tags = value;
@@ -37,6 +42,16 @@ export default class Money extends Vue {
     this.record.notes = value;
   }
 
+  saveRecord() {
+    const record2: RecordItem = model.clone(this.record);
+    record2.createAt = new Date();
+    this.recordList.push(record2);
+  }
+
+  @Watch('recordList')
+  onRecordListChange() {
+    model.save(this.recordList);
+  }
 }
 </script>
 
